2535274280e6b0828ac6aa5dc362f8b7f03e6a10
Martin Gehrke <martin@teamgehrke.com>
2015-10-22 08:54:04 -0400

    initial commit of pentropy

---
+++ python/pentropy.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+# calcaulate the entropy of a string
+# include helpful info and time to crack
+
+import argparse
+import sys
+
+
+
+
+def main(argv=None):
+
+    if argv is None:
+        argv = sys.argv	
+
+
+    parser = argparse.ArgumentParser()
+
+
+    parser.add_argument('-p','--password',help='password to examine',
+                        action='store',dest='passwd',type=str)
+
+    parser.add_argument('-g','--generate',help='generate and examine a new password',
+                        action='store_true',dest='generate')
+
+    args = parser.parse_args(argv[1:])
+
+
+if __name__ == "__main__":
+    sys.exit(not main());
