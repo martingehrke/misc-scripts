cefc261b5787da6f07e14811173c9d2062cd8339
Martin Gehrke <martin@teamgehrke.com>
2017-09-06 14:13:13 -0400

    update to work with multiple devices

--- python/particle-influxdb.py
+++ python/particle-influxdb.py
@@ -6,6 +6,8 @@ import sys
 import urllib3
 import json
 
+urllib3.disable_warnings()
+
 def main(argv=None):
 
     parser = argparse.ArgumentParser()
@@ -16,42 +18,55 @@ def main(argv=None):
 
     args = parser.parse_args(argv[1:])
 
+    URL = "https://api.particle.io/v1/devices/%(DEV)s/%(VAR)s?access_token=%(AT)s"
+
     with open(args.cred_file) as f:
         credentials = [x.strip().split(";") for x in f.readlines()]
 
-    access_token = credentials[0][0]
-    device_id = credentials[0][1]
+
+    devices = {}
+    for credential in credentials:
+        devices[credential[1]] = credential[0]
 
     http = urllib3.PoolManager()
 
     URL = "https://api.particle.io/v1/devices/%(DEV)s/%(VAR)s?access_token=%(AT)s"
 
-    #get humidity
-    r = http.request('GET', URL % {"DEV":device_id, "AT":access_token, "VAR":"humidity"})
-    jdata = json.loads(r.data)
-    humidity = float(jdata["result"])
+    for device in devices.keys():
+        device_id = device
+        access_token = devices[device_id]
 
-    #get temperature
-    r = http.request('GET', URL % {"DEV":device_id, "AT":access_token, "VAR":"temperature"})
-    jdata = json.loads(r.data)
-    temp = float(jdata["result"])
+	r = http.request('GET', "https://api.particle.io/v1/devices/%s?access_token=%s" % (device_id, access_token))
+	jdata = json.loads(r.data)
+	name = jdata["name"]
 
-    db = influxdb.InfluxDBClient("localhost", 8086, "root", "root", "test")
+        #get humidity
+        r = http.request('GET', URL % {"DEV":device_id, "AT":access_token, "VAR":"humidity"})
+        jdata = json.loads(r.data)
+        humidity = float(jdata["result"])
 
-    data = [
-        {
-            "measurement": "temperature",
-            "fields": { "value": temp },
-            "tags": { "device": "particle-1-am2302", "location": "upstairs", "type": "current" },
-        },
-        {
-            "measurement":"humidity",
-            "fields": { "value": humidity },
-            "tags": { "device": "particle-1-am2302", "location": "upstairs", "type": "current" },
-        },
-    ]
+        #get temperature
+        r = http.request('GET', URL % {"DEV":device_id, "AT":access_token, "VAR":"temperature"})
+        jdata = json.loads(r.data)
+        temp = float(jdata["result"])
 
-    db.write_points(data)
+        db = influxdb.InfluxDBClient("localhost", 8086, "root", "root", "test")
+
+        data = [
+                {
+                        "measurement": "temperature",
+                        "fields": { "value": temp },
+                        "tags": { "device": name, "location": "upstairs", "type": "current" },
+                },
+                {
+                        "measurement":"humidity",
+                        "fields": { "value": humidity },
+                        "tags": { "device": name, "location": "upstairs", "type": "current" },
+                },
+        ]
+	
+	print data
+        #db.write_points(data)
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
